737f7e02d6bb8bc8c8ad8e33c6a548e06c23ab26
Jeff <Jeff@bynd.com>
2014-07-16 15:38:59 -0700

    Put a button switch on Sass/Less page. Added js to make it work

---
+++ app/scripts/visSwitch.js
@@ -0,0 +1,21 @@
+/*!
+ * @docauthor Jeff Campbell
+ */
+'use strict';
+
+var i,
+	oldVis,
+	newVis;
+
+function makeVisible(id){
+	oldVis = document.getElementsByClassName('visible');
+	newVis = id;
+	// Setting i length here because of live node list.
+	i = oldVis.length - 1;
+	for(i; i >= 0; i--){
+		// Loops through all previously visible elements and makes them hidden.
+		oldVis[i].className = 'hidden';
+	}
+	// Sets the new element to visible based on id that is passed into function.
+	newVis.className = 'visible';
+}
