dce129912fe1aaa6b8ec1a6eeeb8cf70fd8d1e6d
Jeff <Jeff@bynd.com>
2014-07-15 17:20:22 -0700

    Added a file to start a function for changing an element to fixed after scrolling a certain amount.

---
+++ app/scripts/proScrollAsideFixed.js
@@ -0,0 +1,14 @@
+/*!
+ * @docauthor Jeff Campbell
+ */
+'use strict';
+
+function OnScrollDiv (el) {
+	var height = el.scrollTop;
+	console.log(height);
+	// if(height >= 70){
+	// 	document.getElementById('proAside')
+	// }
+}
+
+OnScrollDiv();
