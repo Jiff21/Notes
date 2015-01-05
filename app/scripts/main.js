4972140d07cccc518216cec90b01a3298b018f89
Jeffrey P Campbell <inajiff@gmail.com>
2015-01-04 19:01:36 -0800

    Added a scroll animate on internal hrefs, also accounted for fixed nav bug that was being ignored on Git and HTML pages for now.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -4,4 +4,18 @@
 'use strict';
 
 
-// console.log('\'Allo \'Allo!');
+// Should I be loading global javascript all in this file?
+// $.ajax({
+//   url: '../Scripts/headerChange.js',
+//   // data: data,
+//   error: function(error) {
+//   	console.log('Hash scoll adjust did not load', error);
+//   },
+
+//   success: function(fileData) {
+//   	console.log('Hash scoll adjust loaded successfully', error);
+//   },
+
+//   dataType: 'js'
+
+// });
