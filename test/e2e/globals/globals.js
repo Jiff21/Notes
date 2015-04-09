66e7458ff3c601ce9c1a2b811ca7f2ed6323f46e
Jeff Campbell <Jeff@bynd.com>
2015-04-09 16:08:30 -0700

    Reallt adding the folder this time.

---
+++ test/e2e/globals/globals.js
@@ -0,0 +1,31 @@
+// Globals and Excludes for jsHint.
+/* global browser */
+/* exported browser */
+
+'use strict';
+
+module.exports = {
+	defaultTimeout: 3000,
+	baseURL: 'http://localhost:9000/Views/',
+	email: 'jeff@example.com',
+	password: 'Password1!',
+	practice: 'Test Practice',
+	organization: 'Test Organization',
+	init: function (browser) {
+	// Initial Navigation
+		return browser
+		.url(browser.globals.baseURL)
+		// Bypass IE6 Certificate Security Error
+		.acceptAlert()
+		// Bypass IE8 Certificate Security Error
+		.getTitle(function(title) {
+			if(title.indexOf('Certificate Error') !== -1) {
+				this.url('javascript:document.getElementById("overridelink").click();');
+			}
+		})
+
+		.maximizeWindow()
+		// Sanity Check - page loaded
+		.waitForElementVisible('body', browser.globals.waitTimeout);
+	}
+};
