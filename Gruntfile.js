925878105088fec11dd1585b8d50954ae1690fcf
Jeff Campbell <Jeff@bynd.com>
2015-10-12 17:27:16 -0700

    updated grunt-image-min, got better logging but still fails on gifs so I turned off gif in min task in gruntfile.

--- Gruntfile.js
+++ Gruntfile.js
@@ -269,7 +269,7 @@ module.exports = function (grunt) {
 				files: [{
 					expand: true,
 					cwd: '<%= config.app %>/images',
-					src: '{,*/}*.{gif,jpeg,jpg,png}',
+					src: '{,*/}*.{jpeg,jpg,png}',
 					dest: '<%= config.dist %>/images'
 				}]
 			}
