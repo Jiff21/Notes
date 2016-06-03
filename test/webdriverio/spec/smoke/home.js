a0c5bd77e91aadf46eb37a2d1e9ad729f596df9d
Jeff Campbell <Jeff@bynd.com>
2016-06-03 11:44:29 -0700

    Chai to match uses regex. Title test written.

--- test/webdriverio/spec/smoke/home.js
+++ test/webdriverio/spec/smoke/home.js
@@ -1,13 +1,14 @@
 'use strict'
 
 describe('The home page', function() {
+
+
 	it('should have a jubotron', function() {
 		browser.url('/Home.html');
-		console.log('Title is: ' + title);
-		browser.getTitle().should.be.equal("Home of Jeff's Note");
-		browser.click('btn-success');
-		// browser.getUrl()
-		
-		// outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
+		var title = browser.getTitle();
+		expect(title).to.match(/Home of Jiff\'s Notes/);
+		// browser.click('btn-success');
 	});
+
+
 });
