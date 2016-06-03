4679f2ba8d03e562d52b02623e970ada63350a8e
Jeff Campbell <Jeff@bynd.com>
2016-06-03 11:17:50 -0700

    wido-run alias.

--- test/webdriverio/spec/smoke/home.js
+++ test/webdriverio/spec/smoke/home.js
@@ -3,11 +3,11 @@
 describe('The home page', function() {
 	it('should have a jubotron', function() {
 		browser.url('/Home.html');
-		// browser.setValue('#search_form_input_homepage', 'WebdriverIO');
-		// browser.click('#search_button_homepage');
-
-		var title = browser.getTitle();
 		console.log('Title is: ' + title);
+		browser.getTitle().should.be.equal("Home of Jeff's Note");
+		browser.click('btn-success');
+		// browser.getUrl()
+		
 		// outputs: "Title is: WebdriverIO (Software) at DuckDuckGo"
 	});
 });
