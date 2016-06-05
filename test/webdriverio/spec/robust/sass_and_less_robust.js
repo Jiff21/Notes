17c622e7c868202975d2c318eabd0b0d86f5231f
Jeff Campbell <jeffrey.patrick.campbell@gmail.com>
2016-06-05 13:48:44 -0700

    Still giving false positive but updated to 3.0 then style.

--- test/webdriverio/spec/robust/sass_and_less_robust.js
+++ test/webdriverio/spec/robust/sass_and_less_robust.js
@@ -5,21 +5,15 @@ describe('The Sass and Less page', function() {
 	it('should have a visible less header after clicking the LESS button', function () {
 		return browser
 			.url('/sass.html')
-			// .waitForVisible('button=LESS', 1000, false)
-			.pause(8000)
 			.click('button=LESS')
-			.pause(2000)
-			.getText('h3#sassy').then(function(text){
-				expect(text).to.contain('LESS')
+			.getText('h3#lassy', function(err, text) {
+				console.log(text);
+				expect(text).to.have.string('LESsS');
 			})
-			// .pause(2000)
-			// .isVisible('h3#lassy').then(function(isVisible){
-			// 	console.log(isVisible)
+			// .isVisible('h3#lassy', function(err, vis){
 			// 	expect(isVisible).to.be.true;
 			// })
-			// .isVisible('h3#sassy').then(function(isVisible){
-			// 	expect(isVisible).to.be.false;
-			// })
-			.call(done);
+			// .call(done);
 	});
 });
+
