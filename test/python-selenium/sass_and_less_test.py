995469d4f5f186717d9a647f0a0c397de3de8538
Jeff Campbell <Jeff@bynd.com>
2016-06-06 16:50:02 -0700

    Forgot to change name of sass test for suite.

--- test/python-selenium/sass_and_less_test.py
+++ test/python-selenium/sass_and_less_test.py
@@ -10,7 +10,7 @@ from globes import *
 
 
 ## Starter test that goes to home, hits Feeeling Lucky Button, then makes sure you're not on Home URL.
-class Less_Button_Test(unittest.TestCase):
+class Less_And_Sass_Buttons(unittest.TestCase):
 
 	def setUp(self):
 		self.drivers = []
@@ -21,7 +21,7 @@ class Less_Button_Test(unittest.TestCase):
 			self.drivers.append(driver)
 		self.verificationErrors = []
 
-	def test_feeling_lucky(self):
+	def less_and_sass_button_show_correct_content(self):
 		for driver in self.drivers:
 			driver.get(globes.base_url + '/sass.html')
 			sass_header = driver.find_element(By.CSS_SELECTOR, 'h3#sassy')
@@ -32,14 +32,12 @@ class Less_Button_Test(unittest.TestCase):
 			less_button = driver.find_element(By.XPATH, '//button[text()="LESS"]')
 			sass_button = driver.find_element(By.XPATH, '//button[text()="SASS"]')
 			less_button.click()
-			driver.implicitly_wait(2)
 			less_header = driver.find_element(By.CSS_SELECTOR, 'h3#lassy')
 			try: self.assertTrue(less_header.is_displayed())
 			except AssertionError, e: self.verificationErrors.append("sass_and_less_test: Less Header not Visible after clicking less button.")
 			try: self.assertFalse(sass_header.is_displayed())
 			except AssertionError, e: self.verificationErrors.append("sass_and_less_test: Sass Header still visible after clicking less button.")
 			sass_button.click()
-			driver.implicitly_wait(2)
 			try: self.assertFalse(less_header.is_displayed())
 			except AssertionError, e: self.verificationErrors.append("sass_and_less_test: Less Header didn't hide after switching back to sass.")
 			try: self.assertTrue(sass_header.is_displayed())
---
+++ test/python-selenium/smoke_suite.py
@@ -0,0 +1,8 @@
+import sys
+import unittest
+from home_test import Homepage_Jumbotron_elements
+from sass_and_less_test import Less_And_Sass_Buttons
+ 
+import unittest
+if __name__ == "__main__":
+	unittest.main()
