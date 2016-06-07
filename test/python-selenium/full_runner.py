cfe8c05db8cb6d51a46aa00e39e7530866548329
Jeff Campbell <jeffrey.patrick.campbell@gmail.com>
2016-06-06 21:18:52 -0700

    Added a test for javascript page.

--- test/python-selenium/full_runner.py
+++ test/python-selenium/full_runner.py
@@ -2,7 +2,8 @@ import sys
 import unittest
 from home_test import *
 from sass_and_less_test import *
- 
+from javascript_page_test import *
+
 import unittest
 if __name__ == "__main__":
 	unittest.main()
