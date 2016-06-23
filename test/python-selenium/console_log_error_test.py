2ad400db0e3cda7555e6edab2a308fa75bc5e5bc
Jeff Campbell <Jeff@bynd.com>
2016-06-23 11:39:48 -0700

    Added a test that looks for severe level logs in console.

---
+++ test/python-selenium/console_log_error_test.py
@@ -0,0 +1,48 @@
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from selenium.webdriver.common.by import By
+from globes import *
+
+# Not working yet. But was trying to test number of elements in the sidebar. 
+class CheckLogsForErrors(unittest.TestCase):
+	def setUp(self):
+		self.drivers = []
+		for driver_instance in globes.desired_cap:
+			driver = webdriver.Remote(
+				command_executor=globes.selenium_server_url,
+				desired_capabilities=driver_instance)
+			self.drivers.append(driver)
+		self.verificationErrors = []
+
+	def test_check_logs_for_error(self):
+		for driver in self.drivers:
+			driver.get(globes.base_url + '/home.html')
+			all_nav_links = []
+			nav_bar_links = driver.find_elements(By.CSS_SELECTOR, '.dropdown-menu > li > a')
+			
+			## Get all links in nav.
+			for current_link in nav_bar_links:
+				# print current_link.get_attribute("href")
+				link_url = current_link.get_attribute("href")
+				all_nav_links.append(link_url)
+
+			try: self.assertTrue(len(all_nav_links) > 0 )
+			except AssertionError, e: self.verificationErrors.append("large_string_window_test: Expected more urls: " + str(len(all_nav_links)))
+			
+			for link in all_nav_links:
+				# print "test_for_overscroll: Getting link " + str(link)
+				driver.get(link)
+				for entry in driver.get_log('browser'):
+					print  entry['level']
+					try: assert "SEVERE" not in entry['level']
+					except AssertionError, e: self.verificationErrors.append("Expeced no errors in log instead got: " + str(entry))
+
+	def tearDown(self):
+		for driver in self.drivers:
+			driver.quit()
+			self.assertEqual([], self.verificationErrors)
+
+
+if __name__ == "__main__":
+	unittest.main()
--- test/python-selenium/globes.py
+++ test/python-selenium/globes.py
@@ -9,8 +9,9 @@ class globes:
 	default_window_position = '200, 200'
 	
 	desired_cap = []
-	desired_cap.append ({'browserName':'chrome', 'javascriptEnabled':'true', 'version':'', 'platform':'ANY'})
-	# desired_cap.append ({'browserName':'firefox', 'javascriptEnabled':'true', 'version':'', 'platform':'ANY'})
-	# desired_cap.append ({'browserName':'safari', 'javascriptEnabled':'true', 'version':'', 'platform':'ANY'})
-	# desired_cap.append ({'browserName':'internet explorer', 'javascriptEnabled':'true', 'version':'9', 'platform':'ANY'})
+	desired_cap.append ({'browserName':'chrome', 'javascriptEnabled':'true', 'version':'', 'platform':'ANY', 'loggingPrefs': { 'browser':'ALL' }})
+	# Not sure if Logging Pref works for all browseers. Investigate later.
+	# desired_cap.append ({'browserName':'safari', 'javascriptEnabled':'true', 'version':'', 'platform':'ANY', 'loggingPrefs': { 'browser':'ALL' }})
+	# desired_cap.append ({'browserName':'firefox', 'javascriptEnabled':'true', 'version':'', 'platform':'ANY', 'loggingPrefs': { 'browser':'ALL' }})
+	# desired_cap.append ({'browserName':'internet explorer', 'javascriptEnabled':'true', 'version':'9', 'platform':'ANY', 'loggingPrefs': { 'browser':'ALL' }})
 	selenium_server_url = 'http://127.0.0.1:4444/wd/hub'
