ca153574b80beab43f1a5328e3075adcdeaa14dc
Jeff Campbell <jeffrey.patrick.campbell@gmail.com>
2016-11-07 19:41:30 -0800

    Trying to remember what I did in list breaker.

--- app/scripts/listBreaker.js
+++ app/scripts/listBreaker.js
@@ -7,7 +7,9 @@
 
 function breakList(numOfLists, list, color){
 	var listLength = list.find('li').size();
+	// console.log('There are this many list items ' + list.find('li').size());
 	var numInRow = Math.ceil(listLength / numOfLists);
+	// console.log('We need to put at least this many in each row' + numInRow);
 	var listNumber = 0;
 
 	for(var b = 0; b < numOfLists; b++) {
@@ -19,16 +21,18 @@ function breakList(numOfLists, list, color){
 
 
 	for (var i = 0; i < numOfLists; i++){
+		// Cuts out numInRow Items to be put in their own list.
 		var listItems = list.find('li').slice(0, numInRow);
 		var newList = $('<ul/>').append(listItems);
 		// var newList = document.createElement('UL').append(listItems);
 
-		if (i%4) {
+		// Increment the row if we're at the splice number
+		if (i%numInRow) {
 			listNumber = listNumber + 1;
 		}
 
-		console.log(document.getElementsByClassName('fourth breaking-list same-list ' + color)[listNumber]);
-
+		// Get the correct div and the appends the new list to it.
+		// console.log(document.getElementsByClassName('fourth breaking-list same-list ' + color)[listNumber]);
 		var currentDiv = document.getElementsByClassName('fourth breaking-list same-list ' + color)[listNumber];
 		// currentDiv.appendChild(newList);
 		$(currentDiv).append(newList);
