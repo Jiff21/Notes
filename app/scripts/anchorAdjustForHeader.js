b2d3f18a4803d73d2117a16faed08715ccee37e8
Jiff21 <jeffreypatrickcampbell@gmail.com>
2016-12-12 16:58:36 -0800

    More cleanup.

--- app/scripts/anchorAdjustForHeader.js
+++ app/scripts/anchorAdjustForHeader.js
@@ -2,22 +2,21 @@
 * Internal anchor links are not taking into
 * account the fixed header, need to adjust 50px.
 */
-'use strict';
-
 /* @flow */
 
+
 $(document).ready(function () {
-	$('a[href*=#]').click(function () {
-		if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '')   && location.hostname === this.hostname) {
-			var $target = $(this.hash);
-			$target = $target.length && $target;
-			if ($target.length) {
-				var targetOffset = $target.offset().top - 60;
-				$('html,body')    .animate({
-					scrollTop: targetOffset
-				}, 1000);
-				return false;
-			}
-		}
-	});
+  $('a[href*=#]').click(function () {
+    if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+      let $target = $(this.hash);
+      $target = $target.length && $target;
+      if ($target.length) {
+        const targetOffset = $target.offset().top - 60;
+        $('html,body').animate({
+          scrollTop: targetOffset
+        }, 1000);
+        return false;
+      }
+    }
+  });
 });
--- app/scripts/newVisSwitch.js
+++ app/scripts/newVisSwitch.js
@@ -8,37 +8,37 @@
 'use strict';
 
 const changeButton = () => {
-	const buttonTitle = document.getElementById('jumboButton');
-	if (buttonTitle.text === 'Newer Examples'){
-		buttonTitle.text = 'Older Examples';
-	} else {
-		buttonTitle.text = 'Newer Examples';
-	}
+  const buttonTitle = document.getElementById('jumboButton');
+  if (buttonTitle.text === 'Newer Examples') {
+    buttonTitle.text = 'Older Examples';
+  } else {
+    buttonTitle.text = 'Newer Examples';
+  }
 };
 
 const changeExample = () => {
-	let i = 0;
-	let l = 0;
-	// HTML Collections live update. Put it in an araay to stop it.	
-	let oldVis = [].slice.call(document.getElementsByClassName('visible'));
-	let newVis = [].slice.call(document.getElementsByClassName('hidden'));
+  let i = 0;
+  let l = 0;
+  // HTML Collections live update. Put it in an araay to stop it.
+  const oldVis = [].slice.call(document.getElementsByClassName('visible'));
+  const newVis = [].slice.call(document.getElementsByClassName('hidden'));
 
-	const numberOfOld = oldVis.length;
-	for(i; i < numberOfOld; i++){
-		// Loops through all previously visible elements and makes them hidden.
-		oldVis[l].classList.add('hidden');
-		oldVis[l].classList.remove('visible');
-	}
-	
-	const numberOfNew = newVis.length;
-	for(l; l < numberOfNew; l++){
-		// Loops through all previously visible elements and makes them hidden.
-		newVis[l].classList.add('visible');
-		newVis[l].classList.remove('hidden');
-	}
+  const numberOfOld = oldVis.length;
+  for (i; i < numberOfOld; i += i) {
+    // Loops through all previously visible elements and makes them hidden.
+    oldVis[l].classList.add('hidden');
+    oldVis[l].classList.remove('visible');
+  }
+
+  const numberOfNew = newVis.length;
+  for (l; l < numberOfNew; l += l) {
+    // Loops through all previously visible elements and makes them hidden.
+    newVis[l].classList.add('visible');
+    newVis[l].classList.remove('hidden');
+  }
 };
 
 const makeVisible = () => {
-	changeButton();
-	changeExample();
+  changeButton();
+  changeExample();
 };
