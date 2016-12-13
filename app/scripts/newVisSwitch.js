511d6517c45330709aaf6ffad9e8124b417af82c
Jiff21 <jeffreypatrickcampbell@gmail.com>
2016-12-13 13:32:07 -0800

    mostly .editorconfig change to 2 spaces.

--- app/scripts/newVisSwitch.js
+++ app/scripts/newVisSwitch.js
@@ -3,8 +3,8 @@
 /*!
  * @docauthor Jeff Campbell
  */
- // Letting jshint know these are Exported Variables.
- /* exported makeVisible */
+// Letting jshint know these are Exported Variables.
+/* exported makeVisible */
 
 const changeButton = () => {
   const buttonTitle = document.getElementById('jumboButton');
@@ -19,21 +19,27 @@ const changeExample = () => {
   let i = 0;
   let l = 0;
   // HTML Collections live update. Put it in an araay to stop it.
-  const oldVis = Array.from(document.getElementsByClassName('visible'));
-  const newVis = Array.from(document.getElementsByClassName('hidden'));
-  // const oldVis = [].slice.call(document.getElementsByClassName('visible'));
-  // const newVis = [].slice.call(document.getElementsByClassName('hidden'));
+  // const oldVis = Array.from(document.getElementsByClassName('visible'));
+  // const newVis = Array.from(document.getElementsByClassName('hidden'));
+  const oldVis = [].slice.call(document.getElementsByClassName('visible'));
+  const newVis = [].slice.call(document.getElementsByClassName('hidden'));
+  console.debug(oldVis.length);
+  console.debug(newVis.length);
 
   const numberOfOld = oldVis.length;
   for (i; i < numberOfOld; i += 1) {
     // Loops through all previously visible elements and makes them hidden.
+    console.debug(oldVis[l]);
     oldVis[l].classList.add('hidden');
     oldVis[l].classList.remove('visible');
   }
 
+  console.debug(oldVis.length);
+  console.debug(newVis.length);
   const numberOfNew = newVis.length;
   for (l; l < numberOfNew; l += 1) {
     // Loops through all previously invisible elements and makes them visible.
+    console.debug(newVis[l]);
     newVis[l].classList.add('visible');
     newVis[l].classList.remove('hidden');
   }
