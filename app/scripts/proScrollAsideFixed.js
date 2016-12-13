d216f9cda7b91d90b0c94e9bfbc991fefa743db7
Jiff21 <jeffreypatrickcampbell@gmail.com>
2016-12-12 17:10:41 -0800

    started linted js again but decided to break.

--- app/scripts/proScrollAsideFixed.js
+++ app/scripts/proScrollAsideFixed.js
@@ -5,26 +5,26 @@
  */
 
 /* exported proAside */
-function grabAsideByID () {
-	return document.getElementById('proAside');
+function grabAsideByID() {
+  return document.getElementById('proAside');
 }
 
-function keepInPlace (el){
-	const scrollAt = 538;
-	const posTop = document.body.scrollTop;
-	if (posTop < scrollAt) {
-		// console.log('removing');
-		el.classList.remove('pro-fixed');
-	} else if (posTop >= scrollAt) {
-		// console.log('adding');
-		el.addClass += 'pro-fixed';
-		el.classList.add('pro-fixed');
-	}
+function keepInPlace(el) {
+  const scrollAt = 538;
+  const posTop = document.body.scrollTop;
+  if (posTop < scrollAt) {
+    // console.log('removing');
+    el.classList.remove('pro-fixed');
+  } else if (posTop >= scrollAt) {
+    // console.log('adding');
+    el.addClass += 'pro-fixed';
+    el.classList.add('pro-fixed');
+  }
 }
 
 window.onload = function(){
-	const proAside = grabAsideByID();
-	window.addEventListener('scroll', function () {
-		keepInPlace(proAside);
-	});
+  const proAside = grabAsideByID();
+  window.addEventListener('scroll', function () {
+    keepInPlace(proAside);
+  });
 };
